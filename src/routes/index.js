7cd21d429f1eb77870fb0ebfe485ec732e0f88b4
Felipe Barros <felipetech@tuta.io>
2020-06-23 12:25:15 -0300

    chore: Add the route of posts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import usersRoute from './users'
+import postsRoute from './posts'
 
 const router = express.Router()
 
 router.use('/users', usersRoute)
+router.use('/posts', postsRoute)
 router.get('/', (req, res) => res.send('Hello World!'))
 
 export default router
---
+++ src/routes/posts.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import PostsController from '../controllers/posts'
+import Post from '../models/Post'
+
+const router = express.Router()
+
+const postsController = new PostsController(Post)
+
+router.get('/', (req, res) => postsController.findAll(req, res))
+router.get('/:id', (req, res) => postsController.getById(req, res))
+router.post('/', (req, res) => postsController.create(req, res))
+router.put('/:id', (req, res) => postsController.update(req, res))
+router.delete('/:id', (req, res) => postsController.delete(req, res))
+
+export default router
